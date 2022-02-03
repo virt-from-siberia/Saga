2d5138e4c5072cf15cf65b14ddb79db8c6966d88
Aleksey <Elchin>
2022-02-03 17:07:38 +0900

    Saga notebook

--- Second/src/redux/reducers/index.js
+++ Second/src/redux/reducers/index.js
@@ -1,28 +1,27 @@
-import {combineReducers} from 'redux';
-import {connectRouter} from 'connected-react-router'
-import { createBrowserHistory } from 'history';
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
 
 export const history = createBrowserHistory();
 
 const initial = {
-    blog: {}
+  blog: {},
 };
 
 export function appReducer(state = initial, action) {
-    switch (action.type) {
-        case 'BLOG_LOADED':
-            return {
-                ...state,
-                blog: action.payload,
-            }
-
-    }
-    return state;
+  switch (action.type) {
+    case "BLOG_LOADED":
+      return {
+        ...state,
+        blog: action.payload,
+      };
+  }
+  return state;
 }
 
 const rootReducer = combineReducers({
-    app: appReducer,
-    router: connectRouter(history),
-})
+  app: appReducer,
+  router: connectRouter(history),
+});
 
 export default rootReducer;
